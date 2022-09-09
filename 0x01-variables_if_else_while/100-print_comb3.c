2ffc7f12426c1e1491b00604bb1334df2126970b
Lornzyy <lornanish@gmail.com>
2022-09-09 11:32:55 -0700

    print out single digits with ,

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible different combinations of two digits.
+ *
+ * Return: Always 0 (Success)
+ */
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,7 +12,7 @@ int num;
 for (num = 0; num <= 9; num++)
 {
 putchar((num % 10) + '0');
-for (num = 0; num < = 8; num ++)
+for (num = 0; num <= 8; num ++)
 putchar(' ');
 putchar(',');
 }
