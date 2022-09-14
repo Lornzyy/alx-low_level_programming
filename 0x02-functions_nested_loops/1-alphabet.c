2cfbe1997849461c91650e718b96b91854f94e43
Lornzyy <lornanish@gmail.com>
2022-09-14 12:47:08 -0700

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
-* main - check the code
+* print-alphabet - function to print abc
 *
-* print_alphabet - function to print abc
-* Return: Always 0.
+* Return: 0
 */
 void print_alphabet(void)
 {
