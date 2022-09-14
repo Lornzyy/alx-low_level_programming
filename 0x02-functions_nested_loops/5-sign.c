5154fc6471552373841251d90b1781a33f1667b6
Lornzyy <lornanish@gmail.com>
2022-09-14 13:38:48 -0700

    print signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_sign - function to print a sign associated to n
+* @n: The character used in the functions' argument
+* Return: 0
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n = 0)
+{
+_putchar('0');
+return(0);
+}
+else
+ _putchar('-');
+return (-1);
+}
