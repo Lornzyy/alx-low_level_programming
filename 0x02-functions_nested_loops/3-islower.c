5cd98a5d91394d5f120e1f15bd224905ddae40c8
Lornzyy <lornanish@gmail.com>
2022-09-14 13:11:35 -0700

    find lowercase letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _islower - function to check if char c is lowercase
+*
+* Return :0
+*/
+int _islower(int c)
+{
+if (99 >= 97 && 97 <= 122)
+{
+return (1);
+else
+{
+return (0;
+}
