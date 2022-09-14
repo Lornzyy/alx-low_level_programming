d5b32050313d43d0f65ebea9959d48ed970e4651
Lornzyy <lornanish@gmail.com>
2022-09-14 15:42:58 -0700

    numbers to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_to_98 - function to print numbers from 0 to 98
+* @n: the int to be used as an argument in the function
+* Return: 0
+*/
+void print_to_98(int n)
+{
+for (n =0; n <=98; n++)
+{
+_putchar(n);
+}
+_putchar(' ');
+_putchar(',');
+}
