54b22740420e6607788e5e5bd31ea599e750c297
Lornzyy <lornanish@gmail.com>
2022-09-15 04:35:51 -0700

    print 0 to 9 with new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers - function to print line 0 to 9 with a new line
+* Return: 0
+*/
+void print_numbers(void)
+{
+int c;
+for ( c = 0; c <= 9; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+return (0); 
+}
