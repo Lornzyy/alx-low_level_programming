6d4909137d051097024edb448fc7b93e44b7c1bb
Lornzyy <lornanish@gmail.com>
2022-09-15 11:07:55 -0700

    print staright line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_line - function to print straight line
+* @n: the int to be used in the argument in the function
+* Return: 0
+*/
+void print_line(int n)
+{
+int a  = 0;
+if (n > 0)
+{
+while (a <= n)
+{
+_putchar('_'_;
+a++;
+}
+_putchar('\n');
+}
+else
+_putchar('\n');
+}
