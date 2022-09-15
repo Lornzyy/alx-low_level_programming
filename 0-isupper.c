d925ff9ec7d4d39d1682633f61308fd9cf7ac3e6
Lornzyy <lornanish@gmail.com>
2022-09-15 03:43:18 -0700

    find if c is uppercase

---
+++ 0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _isupper - function used to find if c is in uppercase
+* @c: the integer to be used an argument for this function
+* Return: 0
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+return (0);
+}
