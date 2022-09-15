f4a7c8c705a651473d2fc9e00b64941a462d863d
Lornzyy <lornanish@gmail.com>
2022-09-15 04:22:12 -0700

    print 0 through 9

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,12 @@
 #include "main.h"
 /**
- * _isdigit - function to check digit 0 through 9
- * @c: The int used to print 0 through 9
- * Return: 0
- */
+* _isdigit - function to check digit 0 through 9
+* @c: The int used to print 0 through 9
+* Return: 0
+*/
 int _isdigit(int c)
 {
-for (c = 0; c < 10; c++)
-{
-if (c > 10)
-{
+if (c >= '0' && c <= '9')
 return (1);
-}
-}
-else
 return (0);
 }
