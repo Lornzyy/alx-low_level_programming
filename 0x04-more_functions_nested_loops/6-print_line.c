d479be832bf78d8b2400ef123ccce8b7e188569d
Lornzyy <lornanish@gmail.com>
2022-09-15 12:04:57 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,16 @@
+#include <stdio.h>
 #include "main.h"
 /**
 * print_line - function that checks for uppercase character.
 * @n: is the integer for the paramaters of my function
-* Return: 0
+* Return: returns mothing
 */
 void print_line(int n)
 {
-int a = 0;
-if (n > 0)
-{
-while (a <= n)
+while (n-- > 0)
 {
 _putchar('_');
-a++;
 }
 _putchar('\n');
 }
-else
-_putchar('\n');
-}
+
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_diagonal - function to print diagonal line
+ * @n: the integer to print the diagonal line
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+  
