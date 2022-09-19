6fa29c197e029b1ed76a6794814f1077a9b19200
Lornzyy <lornanish@gmail.com>
2022-09-19 09:22:15 -0700

    swap two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* swap_int - function to reverse integers
+* @a: parameter to act as first integer
+* @b: parameter to act as second integer
+*/
+void swap_int(int *a, int *b)
+{
+  int temp;
+
+  temp = *a;
+
+  *a = b;
+  *b = temp;
+  
+}
