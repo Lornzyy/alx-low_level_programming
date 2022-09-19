bcff6d8d70776a1aa466c215ba1e99dd0916d5da
Lornzyy <lornanish@gmail.com>
2022-09-19 11:11:49 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_rev - function to print string
+* @s: the string to be printed
+* Return: string
+*/
+
+void print_rev(char *s)
+{
+int length;
+
+while (*(s + length) != '\0')
+{
+length++;
+}
+while (length--)
+{
+putchar(*(s + length)));
+}
+putchar('\n');
+}
