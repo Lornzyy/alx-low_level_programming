3334744d173281daa5eaa64a535f9216f99f759b
Lornzyy <lornanish@gmail.com>
2022-09-19 11:39:43 -0700

    reverse words

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* rev_string - function to reverse words
+* @s : - the parameter to reverse words
+*/
+void rev_string(char *s)
+{
+int i = 0, j = 0;
+char str[500];
+while (*(s + i))
+{
+*(str + i) = *(s + i);
+i++;
+}
+i = i - 1;
+while (i >= 0)
+{
+*(s + i) = *(str + j);
+j++;
+i--;
+}
+}
