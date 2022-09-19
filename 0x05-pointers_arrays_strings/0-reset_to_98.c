8922202daa6ee777f4e9bb3bc2fd91ecc92e51cf
Lornzyy <lornanish@gmail.com>
2022-09-19 09:15:34 -0700

    return 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -8,19 +8,5 @@
 
 void reset_to_98(int *n)
 {
-n = 402;
-}
-
-/**
-* main - function to return 98
-* Return: Always 0
-*/
-int main(void)
-{
-int m;
-
-m = 98;
-reset_to_98(n);
-
-return (0);
+*n = 98;
 }
