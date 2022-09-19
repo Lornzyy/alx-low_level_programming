2f8b080f971a985d331c369eaddee0e270e28902
Lornzyy <lornanish@gmail.com>
2022-09-19 09:11:28 -0700

    return 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,16 +3,24 @@
 * reset_to_98 - function to return 98 using pointers
 *
 * @n: is used as the integer for the function
-* Return: Always 0;
+* Return: Nothing
 */
 
 void reset_to_98(int *n)
 {
+n = 402;
+}
+
+/**
+* main - function to return 98
+* Return: Always 0
+*/
+int main(void)
+{
 int m;
-int *n;
 
 m = 98;
-n = &m;
+reset_to_98(n);
 
 return (0);
 }
