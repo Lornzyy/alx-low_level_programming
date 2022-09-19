2f8d89df2cca4688cd65d1b87de94c4eaf845cdf
Lornzyy <lornanish@gmail.com>
2022-09-19 09:40:11 -0700

    length of char

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * _strlen - function to calculate length of string
- * @s: the string to return lenth of
- */
+* _strlen - function to calculate length of string
+* @s: the string to return lenth of
+* Return: the length of char s
+*/
+
 int _strlen(char *s)
 {
-*s = "trial";
+s = "trial";
 
-printf("The length of 'char' is: %lu\n", sizeof(s));
+printf("The length of 's' is: %ld", strlen(s));
 
+return (0);
 }
