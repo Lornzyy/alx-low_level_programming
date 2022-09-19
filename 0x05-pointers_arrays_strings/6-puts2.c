cc00b2e28785fa8991266e3dbc000cb774a990f6
Lornzyy <lornanish@gmail.com>
2022-09-19 12:27:39 -0700

    print every character in a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,11 +6,10 @@
 */
 void puts2(char *str)
 {
-int i = 0;
-while (*(str + i))
-{
-_putchar(*(str + i));
-i = i + 2;
-}
+int index = 0, len = 0;
+while (str[index++])
+len++;
+for (index = 0; index < len; index += 2)
+_putchar(str[index]);
 _putchar('\n');
 }
