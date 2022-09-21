9b98cd11e288f5e2ca341c986c8e30b841647990
Lornzyy <lornanish@gmail.com>
2022-09-21 01:49:11 -0700

    task o

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - function to concatenate strings
+ * @dest: first param
+ * @src: second param
+ * Return: dest
+ */
+
+char * _strcat(char *dest, char *src)
+{
+int i, j;
+i = j = 0;
+
+while (*(dest +i))
+i++;
+
+while ((*(dest + i) = *(src + j)))
+{
+i++;
+j++;
+}
+
+return (dest);
+}
