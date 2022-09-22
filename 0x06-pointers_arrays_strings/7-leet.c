2905a0899b2085dcdf98ae3afe270cbf8ba00534
Lornzyy <lornanish@gmail.com>
2022-09-22 04:04:42 -0700

    encode string to 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - function to return 1337
+ * @f - param
+ * Return: f
+ */
+
+char *leet(char *f)
+  
+{
+char s[] = "aAeEoOtTlL";
+char p[] = "4433007711";
+
+int i = 0;
+int j = 0;
+
+while (f[i] != '\0')
+{
+for (j = 0; s[j] != '\0' && f[i] != s[j]; j++)
+;
+if (j < 10)
+{
+f[i] = p[j];
+}
+i++;
+}
+return (f);
+}
