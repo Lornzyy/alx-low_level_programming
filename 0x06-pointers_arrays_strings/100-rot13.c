c38e46921cb93f6c5031a62479fb41d09e71b52a
Lornzyy <lornanish@gmail.com>
2022-09-23 03:54:54 -0700

    rot 13 cipher

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,7 +14,7 @@ char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 for (i = 0; (*(s + i) != '\0'); i++)
 {
-for (j = 0; j < 26; j++)
+for (j = 0; j < 52; j++)
 {
 if (*(s + i) == a[j])
 {
