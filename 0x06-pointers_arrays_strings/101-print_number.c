b8a765ae8d1bbb8c243e915bf56dd86d9b3a0933
Lornzyy <lornanish@gmail.com>
2022-09-23 05:15:19 -0700

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,18 +7,26 @@
 
 void print_number(int n)
 {
-unsigned int m;
+unsigned int m, d, count;
+
 if (n < 0)
 {
 _putchar('-');
-n = -n;
+m = n * -1;
 }
 else
-m = n;
 {
-n = n;
+m = n;
 }
-if (n / 10)
-print_number(n / 10);
-_putchar(n % 10 + '0');
+d = m;
+count = 1;
+while (d > 9)
+{
+d /= 10;
+count *= 10;
+}
+for (; count >= 1; count /= 10)
+{
+_putchar(((m / count) % 10) + '0');
+}
 }
