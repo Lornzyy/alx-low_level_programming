00b79ee19f6d3beaf3e27ef2260f1105a91a306d
Lornzyy <lornanish@gmail.com>
2022-09-28 06:05:37 -0700

    string length using recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,17 +4,16 @@
  * _strlen_recursion - find string length using recursion
  * @s: string to find length of
  * Return: 0 when null terminator is encounters
- *         int when the lenth is found
+ *         int when the length is found
  */
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 {
-return 0;
+return (0);
 }
 else
 {
-return 1 + _strlen_recursion(s + 1);
+return (1 + _strlen_recursion(s + 1));
 }
 }
-  
