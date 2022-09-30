7ff2ea5fac24e0b1f99bfdcaff551fa1326b41dd
Lornzyy <lornanish@gmail.com>
2022-09-30 04:01:33 -0700

    name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - function to print name of program
+ * @argc: - count of the cmd aruments
+ * @argv: - array of the cmd arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc >= 1)
+{
+printf("%s\n", argv[0]);
+}
+
+
+return (0);
+}
