e0645f33cb1dddb43339d6071d74a8da724e33d1
Lornzyy <lornanish@gmail.com>
2022-09-30 15:00:03 -0700

    command to create static library

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c libmy.c
-ar rc liball.a libmy.o
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
+ar rc liball.a *.o
+ranlib liballa.a
 
