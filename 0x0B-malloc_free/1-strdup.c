336a6c345bcb53b11d16313224427b8e6ffbda0f
Lornzyy <lornanish@gmail.com>
2022-10-03 05:25:34 -0700

    duplicate string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,47 +1,43 @@
+
 #include "main.h"
 
 /**
- * _strdup - function to copy string
- * @str: string to be copied
+ * _strdup - to print duplicate string
+ * @str - pointer
  * Return: char pointer
  */
 
 char *_strdup(char *str)
 {
 	char *duplicate;
-	int length = 0;
 	int i;
+	int length = 0;
 
-	/* find the length of the string to be copied */
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 
 	while (str[length] != '\0')
 	{
 		length++;
 	}
-
-	/* find the size to be allocated using malloc */
+	/* check for size in bytes using malloc */
 	duplicate = malloc(length * sizeof(char) + 1);
 
-	/* avoid duplicating '\0' */
-	if (duplicate == NULL)
+	/* avoid duplicating a '\0' */
+	if (duplicate  == NULL)
 	{
 		return (NULL);
 	}
 
-	/* DUPLICATE STRING TO LOCATION ASSIGNED */
-
+	/* duplicate the string */
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (duplicate == NULL)
-		{
-			return (NULL);
-		}
 		duplicate[i] = str[i];
 	}
-	duplicate[i] = '\0';
+	duplicate = '\0';
 
 	return (duplicate);
 	free(duplicate);
-
 }
-
