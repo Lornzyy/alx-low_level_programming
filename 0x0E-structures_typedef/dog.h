db523afb973b730c4b6dc5ed3ac5c3af6b00e5fb
Lornzyy <lornanish@gmail.com>
2022-10-11 04:09:15 -0700

    new name type for dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,7 +21,12 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/* typedef dog dog_t; */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 /* void dog_t *new_dog(char *name, float age, char *owner); */
 /*void free_dog(dog_t *d); */
 
