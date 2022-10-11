4a585d96fc6821173f1c5f8a486c3f74abb2ccfa
Lornzyy <lornanish@gmail.com>
2022-10-11 01:59:00 -0700

    initialize dog strust

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define DOG_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - structure to describe dog
@@ -17,4 +18,10 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/* void dog_t *new_dog(char *name, float age, char *owner); */
+/*void free_dog(dog_t *d); */
+
 #endif /* DOG_H */
