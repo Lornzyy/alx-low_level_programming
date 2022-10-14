946b38abd9f3c1b873228d255d4d6c433a34f09d
Lornzyy <lornanish@gmail.com>
2022-10-14 04:20:05 -0700

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,6 +14,10 @@ int sum_them_all(const unsigned int n, ...)
 	va_list(myList);
 	va_start(myList, n);
 
+	if (n == 0)
+	{
+		return (0);
+	}
 	for (i = 0; i < n; i++)
 	{
 		result += va_arg(myList, int);
