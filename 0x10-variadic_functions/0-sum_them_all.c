00c5d6dd92101724460b701b0a774a50b75195f9
Lornzyy <lornanish@gmail.com>
2022-10-14 04:15:50 -0700

    sum them all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum all numbers
+ * @n: const n
+ * Return: int
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int result = 0;
+
+	va_list(myList);
+	va_start(myList, n);
+
+	for (i = 0; i < n; i++)
+	{
+		result += va_arg(myList, int);
+	}
+	va_end(myList);
+
+	return (result);
+}
+
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,3 @@
+0x10. C - Variadic functions
+An introduction to variadic functions 
+
