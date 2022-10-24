5dd9968989bb010477c33827ae4bfc565b4fb4e3
Lornzyy <lornanish@gmail.com>
2022-10-24 05:06:10 -0700

    node

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_lists -print the list and the number of elements
+ * print_list -print the list and the number of elements
  * @h: pointer to the struct list_t
  * Return: list of elements and the number of elements
  */
@@ -15,21 +15,13 @@ size_t print_list(const list_t *h)
 	temp = h;
 	if (temp->str == NULL)
 	{
-		printf("[%d] (nil\n", 0);
+		printf("[%d] (nil)\n", 0);
 	}
 	while (temp->next != NULL)
 	{
-		if (temp->str == NULL)
-		 {
-			 printf("[%d] (nil)\n", 0);
-		 }
 		printf("[%d]%s\n", temp->len, temp->str);
 		count++;
 		temp = temp->next;
-	}
-	while (temp->str == NULL)
-	{
-		printf("[%d] (nil)\n", 0);
 	}
 	printf("[%d]%s\n", temp->len, temp->str);
 	count++;
