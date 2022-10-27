d4bd72b17a7141775a64e5cbb068e210970d0a3d
Lornzyy <lornanish@gmail.com>
2022-10-27 13:07:27 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * list_len - return number of elmenets in the list
+ * listint_len - return number of elmenets in the list
  *
  * @h: pointer to the list_t structure
  * Return: size of the lements
  */
 
-size_t list_len(const list_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t i = 0;
 
