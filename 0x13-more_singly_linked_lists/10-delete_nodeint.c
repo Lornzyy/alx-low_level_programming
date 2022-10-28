c405729f9c8d7bcd5daa8eab51bef0cd5f20a3bd
Lornzyy <lornanish@gmail.com>
2022-10-28 12:41:59 -0700

    task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+
+/**
+ * delete_nodeint_at_index - delete node at index index
+ * @head:pointer to head of the linked lists
+ * @index: index of the node to be deleted
+ * Return: the node to be deleted
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+}
+
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	temp = *head;
 	newnode = (listint_t *)malloc(sizeof(listint_t));
-	if (newnode == NULL || head == NULL)
+	if (!head || !newnode)
 	{
 		return (NULL);
 	}
