b03d69db7e957c44a8d441d3320f39a9c778967f
Lornzyy <lornanish@gmail.com>
2022-10-31 03:16:53 -0700

    binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert binary to unsigned int
+ * @b: the const character to convert
+ * Return: an unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, total = 0, decval = 1;
+	unsigned int slen = strlen(b);
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (i = (slen - 1); i > 0; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+		if (b[i] == '1')
+		{
+			total += decval;
+		}
+		decval *= 2;
+	}
+	return (total);
+}
+
