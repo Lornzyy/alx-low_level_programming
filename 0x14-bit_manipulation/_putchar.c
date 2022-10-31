c322e4bedf388fd52e45383fd5234c99b556eaf2
Lornzyy <lornanish@gmail.com>
2022-10-31 10:08:33 -0700

    final

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <unistd.h>
 
+/**
+ * _putchar - write into a file
+ * @c: the character to be written
+ * Return: int as a character
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
