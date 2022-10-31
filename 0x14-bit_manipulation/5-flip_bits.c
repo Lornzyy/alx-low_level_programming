4098fc9ccb8b61c645f2cf6b07fc341fb2a6ba7d
Lornzyy <lornanish@gmail.com>
2022-10-31 10:10:51 -0700

    final

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,7 +10,7 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int result = n ^ m;
-	unsigned long int flip_num = 0;
+	unsigned int flip_num = 0;
 
 	while (result)
 	{
