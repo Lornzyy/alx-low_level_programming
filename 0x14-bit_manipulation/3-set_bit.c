1ee8003b7003ed00260421e3f0d89ea7ed06c89e
Lornzyy <lornanish@gmail.com>
2022-10-31 05:34:23 -0700

    set value at a specific index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - set value of bit to 1 at a given index
+ * @n: pointer to unsigned long int
+ * @index: index to change value at
+ * Return: integer to be changes
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+			return (-1);
+	*n ^= (1 << index);
+	return (1);
+}
+
