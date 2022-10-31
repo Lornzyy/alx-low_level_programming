e05893e8de41efe34012ee4635dc6b8ef6904db9
Lornzyy <lornanish@gmail.com>
2022-10-31 10:03:05 -0700

    flip through to another number

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - flip through numbers
+ * @n: the first number to flip through
+ * @m the second number to get tp
+ * Return: the number of bits flipped to get to second number
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int result = n ^ m;
+	unsigned long int flip_num = 0;
+
+	while (result)
+	{
+		if (1 & result)
+		{
+			flip_num++;
+		}
+		result = result >> 1;
+	}
+	return (flip_num);
+}
+
