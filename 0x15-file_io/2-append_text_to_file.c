d2c5d07eff3f94d488f2dd6da49f6853ab24722d
Lornzyy <lornanish@gmail.com>
2022-11-02 13:41:14 -0700

    task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file
+ * @text_content: content of the file
+ * Return: the file created
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int _file, _write, len = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	_file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (_file == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		while (text_content[len])
+		{
+			len++;
+		}
+		_write = write(_file, text_content, len);
+		if (_write == -1)
+			return (-1);
+	}
+	close(_file);
+
+	return (1);
+
+}
+
