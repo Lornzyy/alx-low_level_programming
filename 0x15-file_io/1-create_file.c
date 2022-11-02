c582a2e75637bd420cd5863a8ad4464d2c35158b
Lornzyy <lornanish@gmail.com>
2022-11-02 13:20:37 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: name of the file
+ * @text_content: content of the file
+ * Return the file created
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int _file, _write, len = 0;
+
+	if (filename != NULL)
+	{
+		return (-1);
+	}
+
+	_file = open(filename, 0_RDWR | 0_CREAT | 0_TRUNC, 0600);
+	if (_file == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		while (text_content[len])
+		{
+			len++;
+		}
+		_write = write(_file, text_content, len);
+		if(_write == -1)
+			return (-1);
+	}
+	close(_file);
+
+	return (0);
+
+}
+
